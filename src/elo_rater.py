58d534d7c894692e5525a262fd6337819e334ac3
m.pruglo <michael.pruglo.work@gmail.com>
2022-09-14 10:57:53 +0300

    refactor main

--- src/elo_rater.py
+++ src/elo_rater.py
@@ -5,23 +5,21 @@ from elo_rater_types import Outcome
 from elo_rater_view import EloGui
 from elo_rater_model import EloCompetition
 
-def main():
+
+def setup_logger(media_dir):
+  log_filename = f"matches_{media_dir.split('/')[-1]}.log"
+  print(f"logging to file {log_filename}")
+
   logging.basicConfig(
     handlers = [
       logging.StreamHandler(),
-      logging.FileHandler("matches.log", encoding="utf-8")
+      logging.FileHandler(log_filename, encoding="utf-8")
     ],
     format = "%(asctime)s %(message)s",
     datefmt = "%Y-%m-%d %H:%M",
     level = logging.INFO
   )
   logging.info("Starting new session...")
-
-  media_dir = sys.argv[1] if len(sys.argv)>1 else "./test_imgs/"
-  media_dir = os.path.abspath(media_dir)
-  assert os.path.exists(media_dir), f"path {media_dir} doesn't exist"
-
-  run_rater(media_dir)
 
 def run_rater(media_dir):
   gui = EloGui()
@@ -39,4 +37,9 @@ def run_rater(media_dir):
 
 
 if __name__ == "__main__":
-  main()
+  media_dir = sys.argv[1] if len(sys.argv)>1 else "./test_imgs/"
+  media_dir = os.path.abspath(media_dir)
+  assert os.path.exists(media_dir), f"path {media_dir} doesn't exist"
+
+  setup_logger(media_dir)
+  run_rater(media_dir)
