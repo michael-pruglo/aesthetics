9a54390c75bf6b1994bc64db6f6ce800614e48b8
m.pruglo <michael.pruglo.work@gmail.com>
2022-09-21 09:26:03 +0300

    disable logging during tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,12 @@
 import os
 import sys
+import logging
+
+
 PROJECT_PATH = os.getcwd()
 SOURCE_PATH = os.path.join(
     PROJECT_PATH,"src"
 )
 sys.path.append(SOURCE_PATH)
+
+logging.disable(logging.CRITICAL)
