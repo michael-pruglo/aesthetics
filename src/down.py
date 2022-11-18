9519d97ab80299ec9a14c18c814fae4834e042f9
m.pruglo <michael.pruglo.work@gmail.com>
2022-11-18 09:30:59 +0200

    refactor

--- src/down.py
+++ src/down.py
@@ -1,46 +1,12 @@
 #!/usr/bin/env python3
 
-import tkinter as tk
-
-from gui.meta_editor import MetaEditor
-from ae_rater_types import ManualMetadata, ProfileInfo, UserListener
-from db_managers import get_vocab
 import helpers as hlp
-from ai_assistant import Assistant
-from metadata import write_metadata
 from udownloader import UDownloader
-from secretcfg import UDOWNLOADER_CFG
-
-
-class MockListener(UserListener):
-  def consume_result(self, *args, **kwargs): pass
-  def give_boost(self, *args, **kwargs): pass
-  def start_next_match(self): pass
-  def search_for(self, *args, **kwargs): pass
-  def suggest_tags(self, fullname: str) -> list:
-    return Assistant().suggest_tags(fullname)
-  def update_meta(self, fullname:str, meta:ManualMetadata) -> None:
-    print("update_meta: ", fullname, meta)
-    write_metadata(fullname, meta.tags, meta.stars, append=False)
-
-
-def show_editor(fullname:str):
-  root = tk.Tk()
-  root.withdraw()
-
-  meditor = MetaEditor(root, get_vocab(), MockListener())
-  meditor.set_curr_profile(ProfileInfo(fullname))
-  window = meditor.open(None)
-
-  def on_destroy(e):
-    if e.widget == window:
-      root.quit()
-  window.bind('<Destroy>', on_destroy)
-  window.mainloop()
-  root.destroy()
+from down_gui import DownGui
 
 def main(cfg):
   udownloader = UDownloader(cfg)
+  gui = DownGui()
   while True:
     usr_input = input("> ")
     if usr_input.lower() == "exit":
@@ -51,8 +17,9 @@ def main(cfg):
     if ext in ['png','webp','webm']:
       print(f"unsupported file extension '{ext}'")
     else:
-      show_editor(fullname)
+      gui.show_editor(fullname)
 
 
 if __name__ == "__main__":
+  from secretcfg import UDOWNLOADER_CFG
   main(UDOWNLOADER_CFG)
---
+++ src/down_gui.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+from gui.meta_editor import MetaEditor
+from ae_rater_types import ManualMetadata, ProfileInfo, UserListener
+from db_managers import get_vocab
+from ai_assistant import Assistant
+from metadata import write_metadata
+
+
+class MockListener(UserListener):
+  def consume_result(self, *args, **kwargs): pass
+  def give_boost(self, *args, **kwargs): pass
+  def start_next_match(self): pass
+  def search_for(self, *args, **kwargs): pass
+  def suggest_tags(self, fullname: str) -> list:
+    return Assistant().suggest_tags(fullname)
+  def update_meta(self, fullname:str, meta:ManualMetadata) -> None:
+    print("update_meta: ", fullname, meta)
+    write_metadata(fullname, meta.tags, meta.stars, append=False)
+
+class DownGui:
+  def __init__(self) -> None:
+    self.root = tk.Tk()
+    self.root.withdraw()
+
+  def show_editor(self, fullname:str):
+    meditor = MetaEditor(self.root, get_vocab(), MockListener())
+    meditor.set_curr_profile(ProfileInfo(fullname))
+    window = meditor.open(None)
+
+    def on_destroy(e):
+      if e.widget == window:
+        self.root.quit()
+    window.bind('<Destroy>', on_destroy)
+    window.mainloop()
